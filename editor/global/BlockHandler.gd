5cd654077d3da7a3fc6e2a8f3e57adbdce909153
FlowVix <flowvix@gmail.com>
2021-07-27 02:38:43 +0300

    changed and moved block definitions to prepare for code conversion

--- editor/global/BlockHandler.gd
+++ editor/global/BlockHandler.gd
@@ -143,191 +143,16 @@ const COMPONENT_NAMES = {
 	"any": Components.UNIVERSAL,
 }
 
-
-const BLOCK_DEFINITIONS = [
-	
-	#================================================
-	###format:
-	#"BLOCK_ID|block_type|category|definition"
-	
-	##definition:
-	
-	#%component(unique_name)
-	#   in case of %list -> %list(unique_name)[a,b,c] for options
-	
-	#text inbetween has whitespace removed at the edges
-	#================================================
-	
-	####CONTROL
-	#------------------------------------------------
-	
-	"IF|statement|control|if %bool(condition) %open(true)",
-	"IF_ELSE|statement|control|if %bool(condition) %open(true) else %open(false)",
-	
-	"FOR_IN|statement|control|if %bool(condition) %open(true) else %open(false)",
-	
-	"WHILE|statement|control|while %bool(condition) %open(blocks)",
-	
-	"REPEAT|statement|control|repeat %n(amount) times %open(blocks)",
-	
-	
-	####GROUPS
-	#------------------------------------------------
-	"MOVE_INSTANT|statement|groups|move %g(group) x: %n(x) y: %n(y)",
-	"MOVE_DURATION|statement|groups|move %g(group) x: %n(x) y: %n(y) over %n(time) seconds",
-	"MOVE_FULL|statement|groups|move %g(group) x: %n(x) y: %n(y) over %n(time) seconds, easing: %list(easing)[Ease In Out, Ease In, Ease Out, Elastic In Out, Elastic In, Elastic Out, Bounce In Out, Bounce In, Bounce Out, Exponential In Out, Exponential In, Exponential Out, Sine In Out, Sine In, Sine Out, Back In Out, Back In, Back Out] rate: %n(rate)",
-	"ALPHA|statement|groups|set alpha of %g(group) to %n(amount) `%",
-	
-	"FOLLOW|statement|groups|make %g(group1) follow %g(group2) for %n(time) seconds",
-	"FOLLOW_FULL|statement|groups|make %g(group1) follow %g(group2) for %n(time) seconds, x mod: %n(x_mod) y mod: %n(y_mod)",
-	
-	"FOLLOW_Y|statement|groups|make %g(group1) follow the player's Y for %n(time) seconds",
-	"FOLLOW_Y_DELAY|statement|groups|make %g(group1) follow the player's Y for %n(time) seconds with a %n(delay) second delay",
-	"FOLLOW_Y_DELAY|statement|groups|make %g(group1) follow the player's Y for %n(time) seconds with a %n(delay) second delay, offset: %n(offset) max speed: %n(max_speed)",
-	
-	"LOCK_TO_PLAYER_X|statement|groups|lock %g(group1) to the player's X for %n(time) seconds",
-	"LOCK_TO_PLAYER_Y|statement|groups|lock %g(group1) to the player's Y for %n(time) seconds",
-	"LOCK_TO_PLAYER|statement|groups|lock %g(group1) to the player for %n(time) seconds",
-	
-	"MOVE_TO_INSTANT|statement|groups|move %g(group1) to %g(group2)",
-	"MOVE_TO_DURATION|statement|groups|move %g(group1) to %g(group2) over %n(time) seconds",
-	"MOVE_TO_FULL|statement|groups|move %g(group1) to %g(group2) over %n(time) seconds, easing: %list(easing)[Ease In Out, Ease In, Ease Out, Elastic In Out, Elastic In, Elastic Out, Bounce In Out, Bounce In, Bounce Out, Exponential In Out, Exponential In, Exponential Out, Sine In Out, Sine In, Sine Out, Back In Out, Back In, Back Out] rate: %n(rate)",
-	
-	"ROTATE_INSTANT|statement|groups|rotate %g(group1) %n(degrees) degrees around %g(group2) (lock rotation? %bool(lock))",
-	"ROTATE_DURATION|statement|groups|rotate %g(group1) %n(degrees) degrees around %g(group2) over %n(time) seconds (lock rotation? %bool(lock))",
-	"ROTATE_FULL|statement|groups|rotate %g(group1) %n(degrees) degrees around %g(group2) over %n(time) seconds, easing: %list(easing)[Ease In Out, Ease In, Ease Out, Elastic In Out, Elastic In, Elastic Out, Bounce In Out, Bounce In, Bounce Out, Exponential In Out, Exponential In, Exponential Out, Sine In Out, Sine In, Sine Out, Back In Out, Back In, Back Out] rate: %n(rate) (lock rotation? %bool(lock))",
-	
-	"TOGGLE_OFF|statement|groups|toggle %g(group) off",
-	"TOGGLE_ON|statement|groups|toggle %g(group) on",
-	
-	"PULSE_GROUP|statement|groups|pulse %g(group) %pick(color) fade in: %n(fade_in) hold: %n(hold) fade out: %n(fade_out) exclusive? %bool(exclusive)",
-	"PULSE_GROUP_RGB|statement|groups|pulse %g(group) r: %n(r) g: %n(g) b: %n(b) fade in: %n(fade_in) hold: %n(hold) fade out: %n(fade_out) exclusive? %bool(exclusive)",
-	"PULSE_GROUP_HSV|statement|groups|pulse %g(group) h: %n(r) s: %n(g) %bool(s_checked) v: %n(b) %bool(v_checked) fade in: %n(fade_in) hold: %n(hold) fade out: %n(fade_out) exclusive? %bool(exclusive)",
-	
-	####COLORS
-	#------------------------------------------------
-	
-	"PULSE_COLOR|statement|colors|pulse %c(channel) %pick(color) fade in: %n(fade_in) hold: %n(hold) fade out: %n(fade_out) exclusive? %bool(exclusive)",
-	"PULSE_COLOR_RGB|statement|colors|pulse %c(channel) r: %n(r) g: %n(g) b: %n(b) fade in: %n(fade_in) hold: %n(hold) fade out: %n(fade_out) exclusive? %bool(exclusive)",
-	"PULSE_COLOR_HSV|statement|colors|pulse %c(channel) h: %n(r) s: %n(g) %bool(s_checked) v: %n(b) %bool(v_checked) fade in: %n(fade_in) hold: %n(hold) fade out: %n(fade_out) exclusive? %bool(exclusive)",
-	
-	"SET_COLOR|statement|colors|set %c(channel) to %pick(color) over %n(time) seconds, blending? %bool(blending)",
-	"SET_COLOR_RGBA|statement|colors|set %c(channel) to r: %n(r) g: %n(g) b: %n(b) a: %n(opacity) over %n(time) seconds, blending? %bool(blending)",
-	
-	####BLOCKS
-	#------------------------------------------------
-	
-	"TRACKER_COUNTER|counter|blocks|tracker counter for %b(a) and %b(b)",
-	
-	####COUNTERS
-	#------------------------------------------------
-	
-	"ADD|statement|counters|add %n(value) to %#(counter)",
-	"SUBTRACT|statement|counters|subtract %n(value) from %#(counter)",
-	"MULTIPLY|statement|counters|multiply %#(counter) by %n(value)",
-	"DIVIDE|statement|counters|divide %#(counter) by %n(value)",
-	"ASSIGN|statement|counters|set %#(counter) to %n(value)",
-	
-	####STRINGS
-	#------------------------------------------------
-	
-	"JOIN|string|strings|join %str(a) %str(b)",
-	"LETTER|string|strings|letter %n(i) of %str(text)",
-	"LENGTH|string|strings|length of %str(text)",
-
-	"STRING_CONTAINS|boolean|strings|%str(text1) contains %str(text2)",
-	"STRING_STARTS|boolean|strings|%str(text1) starts with %str(text2)",
-	"STRING_ENDS|boolean|strings|%str(text1) ends with %str(text2)",
-	"STRING_INDEX|number|strings|position of %str(text2) in %str(text2)",
-	"STRING_IS_EMPTY|boolean|strings|is %str(text) empty",
-	"STRING_IS_LOWERCASE|boolean|strings|is %str(text) lowercase",
-	"STRING_IS_UPPERCASE|boolean|strings|is %str(text) uppercase",
-	
-	"STRING_INTERLACE|string|strings|interlace %str(text) in %arr(array)",
-	
-	"STRING_LOWERCASE|string|strings|make %str(text) lowercase",
-	"STRING_UPPERCASE|string|strings|make %str(text) uppercase",
-	
-	"STRING_REVERSE|string|strings|reverse %str(text)",
-	"STRING_SPLIT|array|strings|split %str(text) with %str(separator)",
-	
-	"SUBSTRING|string|strings|part of %str(text) from %n(a) to %n(b)",
-	
-	####ARRAYS
-	#------------------------------------------------
-	
-	"CLEAR_ARRAY|statement|arrays|clear %arr(array)",
-	"ARRAY_CONTAINS|boolean|arrays|%arr(array) contains %any(el)",
-	"ARRAY_INDEX|number|arrays|position of %any(el) in %arr(array)",
-	"ARRAY_EMPTY|boolean|arrays|is %arr(array) empty",
-	"ARRAY_MAX|number|arrays|max number in %arr(array)",
-	"ARRAY_MIN|number|arrays|min number in %arr(array)",
-	"ARRAY_POP|statement|arrays|remove last element from %arr(array)",
-	"ARRAY_SHIFT|statement|arrays|remove first element from %arr(array)",
-	"ARRAY_REMOVE|statement|arrays|remove element at position %n(pos) from %arr(array)",
-	"ARRAY_PUSH|statement|arrays|add %any(el) to %arr(array)",
-	"ARRAY_REVERSE|statement|arrays|reverse %arr(array)",
-	"ARRAY_SORT|array|arrays|sort %arr(array)",
-	"ARRAY_SUM|number|arrays|sum of numbers in %arr(array)",
-	"ARRAY_UNSHIFT|statement|arrays|add %any(el) to the start of %arr(array)",
-	
-	"RANGE|array|arrays|range from %n(start) to %n(end)",
-	"RANGE_STEP|array|arrays|range from %n(start) to %n(end) with step %n(step)",
-	
-	"TYPE_RANGE|array|arrays|%list(type)[group,color,block,item] range from %n(start) to %n(end)",
-	"TYPE_RANGE_STEP|array|arrays|%list(type)[group,color,block,item] range from %n(start) to %n(end) with step %n(step)",
-	
-	####OBJECTS
-	#------------------------------------------------
-	
-	"OBJECT_ADD|statement|objects|add %obj(object) to level",
-	"OBJECT_SET|statement|objects|set key %str(key) of %obj(object) to %any(value)",
-	
-	####OPERATORS
-	#------------------------------------------------
-	
-	"PLUS|number|operators|%n(a) + %n(b)",
-	"MINUS|number|operators|%n(a) - %n(b)",
-	"MULT|number|operators|%n(a) * %n(b)",
-	"DIV|number|operators|%n(a) / %n(b)",
-	"MOD|number|operators|%n(a) `% %n(b)",
-	
-	"POW|number|operators|%n(a) ^ %n(b)",
-	
-	"ABS|number|operators|abs %n(x)",
-	
-	"TRIG|number|operators|%list(op)[sin,cos,tan,asin,acos,atan] %n(x)",
-	"ROUND|number|operators|%list(op)[round,floor,ceil] %n(x)",
-	"EULER|number|operators|%list(op)[e^,ln] %n(x)",
-	"SQRT|number|operators|sqrt %n(x)",
-	
-	"NOT|boolean|operators|not %bool(a)",
-	"AND|boolean|operators|%bool(a) and %bool(b)",
-	"OR|boolean|operators|%bool(a) or %bool(b)",
-	"XOR|boolean|operators|%bool(a) xor %bool(b)",
-	
-	"EQUALS|boolean|operators|%n(a) = %n(b)",
-	"GREATER|boolean|operators|%n(a) > %n(b)",
-	"LESSER|boolean|operators|%n(a) < %n(b)",
-	"GREATER_EQ|boolean|operators|%n(a) ≥ %n(b)",
-	"LESSER_EQ|boolean|operators|%n(a) ≤ %n(b)",
-	
-]
-
-
 func parseDef(var def):
 	
-	var block_ID = ""
+	
 	var block_type = ""
 	var category = ""
 	
 	var components = []
 	
 	var i = 0
-	while def[i] != '|':
-		block_ID += def[i]
-		i += 1
-	i += 1
+	
 	while def[i] != '|':
 		block_type += def[i]
 		i += 1
@@ -390,7 +215,7 @@ func parseDef(var def):
 		components.append({"type": Components.TEXT, "text": new_text.strip_edges()})
 		new_text = ""
 	
-	BLOCK_DEFINITIONS_PARSED[block_ID] = {
+	return {
 		"block_type": block_type,
 		"category": category,
 		"components": components
@@ -398,8 +223,11 @@ func parseDef(var def):
 	
 func _ready():
 	
-	for i in BLOCK_DEFINITIONS:
-		parseDef(i)
+	for i in Blocks.BLOCK_DEFINITIONS:
+		BLOCK_DEFINITIONS_PARSED[i] = parseDef(Blocks.BLOCK_DEFINITIONS[i]["def"])
+	
+	
+	
 		
 const BLOCK_DEFINITIONS_PARSED = {
 		
